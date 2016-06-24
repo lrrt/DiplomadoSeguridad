ce64ad76dac2e94d8bf6581312bcf353e7df8f96
lrrt <lidiaruby.rt@gmail.com>
2016-06-24 17:40:04 -0500

    AppWebDiplomado
    Aplicación web

---
+++ dirprueba/dirprueba/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dirprueba.MvcApplication" Language="C#" %>
---
+++ dirprueba/dirprueba/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dirprueba.Startup))]
+namespace dirprueba
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
